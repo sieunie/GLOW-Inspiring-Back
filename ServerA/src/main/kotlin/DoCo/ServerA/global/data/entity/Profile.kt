0bf0ed6bbbdbc1fcac772baadfa750e003156e00
anjdydhody <a01066444534@gmail.com>
2024-05-30 23:43:01 +0900

    fix: change profile id

--- ServerA/src/main/kotlin/DoCo/ServerA/global/data/entity/Profile.kt
+++ ServerA/src/main/kotlin/DoCo/ServerA/global/data/entity/Profile.kt
@@ -2,13 +2,11 @@ package DoCo.ServerA.global.data.entity
 
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
-import jakarta.persistence.OneToOne
 
 @Entity
 data class Profile(
     @Id
-    @OneToOne
-    var user: User,
+    var userId: Long,
     var description: String?,
     var image: String?,
     var education: String?
