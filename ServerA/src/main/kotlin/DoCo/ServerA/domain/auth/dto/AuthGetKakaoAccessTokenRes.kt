951e98aee2403b4f5247c749e3f389d8e8c1ae7b
Seungkyu-Han <trust1204@gmail.com>
2024-06-01 14:54:19 +0900

    feat: add Login API

---
+++ ServerA/src/main/kotlin/DoCo/ServerA/domain/auth/dto/AuthGetKakaoAccessTokenRes.kt
@@ -0,0 +1,5 @@
+package DoCo.ServerA.domain.auth.dto
+
+data class AuthGetKakaoAccessTokenRes (
+    val access_token: String
+)
--- ServerA/src/main/kotlin/DoCo/ServerA/domain/auth/presentation/AuthController.kt
+++ ServerA/src/main/kotlin/DoCo/ServerA/domain/auth/presentation/AuthController.kt
@@ -1,14 +1,28 @@
 package DoCo.ServerA.domain.auth.presentation
 
 import DoCo.ServerA.domain.auth.application.AuthService
+import DoCo.ServerA.domain.auth.dto.AuthGetLoginRes
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/auth")
 @Tag(name = "인증")
 class AuthController(
-    private val authService: AuthService
-) {
+    private val authService: AuthService) {
+
+    @GetMapping("/login")
+    @ApiResponses(
+        ApiResponse()
+    )
+    fun getLogin(@RequestParam code: String): ResponseEntity<AuthGetLoginRes>{
+        return authService.getLogin(code)
+    }
+
 }
