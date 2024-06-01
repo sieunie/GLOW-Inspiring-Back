e9eda04615e9a450c6712dc3c3374b94ea5b0c1d
Seungkyu-Han <trust1204@gmail.com>
2024-06-01 19:32:44 +0900

    feat: add Profile API

--- ServerA/src/main/kotlin/DoCo/ServerA/domain/profile/presentation/ProfileController.kt
+++ ServerA/src/main/kotlin/DoCo/ServerA/domain/profile/presentation/ProfileController.kt
@@ -12,8 +12,10 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.Authentication
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -32,15 +34,44 @@ class ProfileController(
         ApiResponse(responseCode = "200", description = "성공", content = arrayOf(Content()))
     )
     fun post(@RequestBody profilePostReq: ProfilePostReq, @Parameter(hidden = true) authentication: Authentication): ResponseEntity<HttpStatus> {
-        return profileService.post(profilePostReq, authentication);
+        return profileService.post(profilePostReq, authentication)
     }
 
     @GetMapping
     @Operation(summary = "프로필 조회")
     @ApiResponses(
-        ApiResponse(responseCode = "200", description = "성공", content = arrayOf(Content()))
+        ApiResponse(responseCode = "200", description = "성공", content = arrayOf(Content())),
+        ApiResponse(responseCode = "400", description = "프로필 없음")
     )
     fun get(@RequestParam id: Long): ResponseEntity<ProfileGetRes> {
         return profileService.get(id)
+    }
+
+    @GetMapping("/my")
+    @Operation(summary = "본인 프로필 조회")
+    @ApiResponses(
+        ApiResponse(responseCode = "200", description = "성공", content = arrayOf(Content())),
+        ApiResponse(responseCode = "400", description = "프로필 없음")
+    )
+    fun get(@Parameter(hidden = true) authentication: Authentication): ResponseEntity<ProfileGetRes>{
+        return profileService.get(authentication)
+    }
+
+    @PutMapping
+    @Operation(summary = "프로필 수정")
+    @ApiResponses(
+        ApiResponse(responseCode = "200", description = "성공")
+    )
+    fun put(@RequestBody profilePostReq: ProfilePostReq, @Parameter(hidden = true) authentication: Authentication): ResponseEntity<HttpStatus>{
+        return profileService.put(profilePostReq, authentication)
+    }
+
+    @DeleteMapping
+    @Operation(summary = "프로필 삭제")
+    @ApiResponses(
+        ApiResponse(responseCode = "200", description = "성공")
+    )
+    fun delete(@Parameter(hidden = true) authentication: Authentication): ResponseEntity<HttpStatus>{
+        return profileService.delete(authentication)
     }
 }
--- ServerA/src/main/kotlin/DoCo/ServerA/global/repository/ProfileRepository.kt
+++ ServerA/src/main/kotlin/DoCo/ServerA/global/repository/ProfileRepository.kt
@@ -3,7 +3,10 @@ package DoCo.ServerA.global.repository
 import DoCo.ServerA.global.data.entity.Profile
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 
 @Repository
 interface ProfileRepository: JpaRepository<Profile, Long> {
+    @Transactional
+    fun deleteByUserId(userId: Long)
 }
